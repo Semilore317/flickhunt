80d3e9c5ca7d24885a41c3e6482a5e02c24c48d7
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-03-14 21:56:42 +0100

    finished hero section

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
     return (
         <main>
             <div className="pattern">
-                <h3 className="text-6xl font-bold text-center mt-4 text-gradient">FLickhunt</h3>
+                <h3 className="text-6xl font-bold text-gradient sm:p-5 sm:text-center md:text-left md:p-8">FLickhunt</h3>
             </div>
             <div className="wrapper">
                 <header>
@@ -15,8 +15,8 @@ const App = () => {
                     <h1>Find <span className="text-gradient">Movies</span> You Love</h1>
                 </header>
 
-
                 <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                <h1 className="text-white">{searchTerm}</h1>
             </div>
         </main>
     )
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import searchImage from "/public/search.svg"
 
 const Search = ({searchTerm, setSearchTerm}) => {
     return (
-        <div className="text-white text-3xl">
-            {searchTerm}
-        </div>
+    <div className="relative w-full max-w-lg mx-auto">
+        <input
+            type="text"
+            placeholder="Search for your favorite movie"
+            className="w-full px-5 py-3 pl-12 text-white bg-[rgba(225,225,225,0.2)] rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 placeholder-gray-400"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+            src={searchImage}
+            alt="Search"
+            className="absolute left-4 top-3.5 h-5 w-5"
+        />
+    </div>
     )
 }
 export default Search
